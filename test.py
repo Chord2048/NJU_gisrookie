fa1347d6557d26f07b0e219d46ada4dc14c18be9
chord <0204@chrod.com>
2021-10-27 12:31:25 +0800

    upset

--- test.py
+++
@@ -1,2 +0,0 @@
-import math as m
-print(dir(m))
--- test1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-ls = np.array([[1, 2], [3, 4]])
-print(ls)
-print(ls[0][1]+ls[1][0])
-
-
-arr = np.array([[1, 2, 3, 4, 5], [6, 10, 8, 9, 10], [6, 7, 8, 9, 10]])
-
-print(arr[1:3, 1:4])
